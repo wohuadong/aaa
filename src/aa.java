e012b9be47725d044bbc1b66295d9d115dcc5a76
dh <2472072904@qq.com>
2020-11-05 16:07:33 +0800

    xxx

--- src/aa.java
+++ src/aa.java
@@ -1,10 +1,6 @@
 public class aa {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println(123);
-        System.out.println(345);
-        System.out.println(678);
-        System.out.println(111);
     }
 }
 
